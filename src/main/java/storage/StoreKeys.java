21e2738cd3fcdd94cc9e51253783716c66003955
Michael Kyeyune <kyymic001@nigeria.cs.uct.ac.za>
2016-04-01 13:32:14 +0200

    Added names of files to which keys are stored

--- src/main/java/storage/StoreKeys.java
+++ src/main/java/storage/StoreKeys.java
@@ -15,20 +15,26 @@ public class StoreKeys
     public static final String CLIENT_KEYS_FOLDER = "src/main/resources/keys/client/";
     public static final String SERVER_KEYS_FOLDER = "src/main/resources/keys/server/";
 
+    public static final String CLIENT_PUBLIC_KEY_FILE_NAME = "client-public.store";
+    public static final String CLIENT_PRIVATE_KEY_FILE_NAME = "client-private.store";
+
+    public static final String SERVER_PUBLIC_KEY_FILE_NAME = "server-public.store";
+    public static final String SERVER_PRIVATE_KEY_FILE_NAME = "server-private.store";
+
     public static void main(String[] args)
     {
         KeyPair clientKeyPair = RSA.generateKeyPair(2048);
         KeyPair serverKeyPair = RSA.generateKeyPair(2048);
 
         //save both client and server public keys in their key collections
-        STORE.savePublicKeyToFile(CLIENT_KEYS_FOLDER + "client-public.store", clientKeyPair.getPublic());
-        STORE.savePublicKeyToFile(SERVER_KEYS_FOLDER + "client-public.store", clientKeyPair.getPublic());
+        STORE.savePublicKeyToFile(CLIENT_KEYS_FOLDER + CLIENT_PUBLIC_KEY_FILE_NAME, clientKeyPair.getPublic());
+        STORE.savePublicKeyToFile(SERVER_KEYS_FOLDER + CLIENT_PUBLIC_KEY_FILE_NAME, clientKeyPair.getPublic());
 
-        STORE.savePublicKeyToFile(SERVER_KEYS_FOLDER + "server-public.store", serverKeyPair.getPublic());
-        STORE.savePublicKeyToFile(CLIENT_KEYS_FOLDER + "server-public.store", serverKeyPair.getPublic());
+        STORE.savePublicKeyToFile(SERVER_KEYS_FOLDER + SERVER_PUBLIC_KEY_FILE_NAME, serverKeyPair.getPublic());
+        STORE.savePublicKeyToFile(CLIENT_KEYS_FOLDER + SERVER_PUBLIC_KEY_FILE_NAME, serverKeyPair.getPublic());
 
         //save the private keys in only the expected key collections
-        STORE.savePrivateKeyToFile(CLIENT_KEYS_FOLDER + "client-private.store", clientKeyPair.getPrivate());
-        STORE.savePrivateKeyToFile(SERVER_KEYS_FOLDER + "server-private.store", serverKeyPair.getPrivate());
+        STORE.savePrivateKeyToFile(CLIENT_KEYS_FOLDER + CLIENT_PRIVATE_KEY_FILE_NAME, clientKeyPair.getPrivate());
+        STORE.savePrivateKeyToFile(SERVER_KEYS_FOLDER + SERVER_PRIVATE_KEY_FILE_NAME, serverKeyPair.getPrivate());
     }
 }
