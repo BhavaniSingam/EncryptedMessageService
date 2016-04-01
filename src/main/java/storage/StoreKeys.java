6f9878bb6992a9fc7a3b3eb5158dec5e47dc0daa
Michael Kyeyune <kyymic001@nigeria.cs.uct.ac.za>
2016-04-01 13:11:38 +0200

    Created class to handle saving public and private keys of the client and server to their respective collections

---
+++ src/main/java/storage/StoreKeys.java
@@ -0,0 +1,34 @@
+package storage;
+
+import encryption.RSA;
+
+import java.security.KeyPair;
+
+/**
+ * <h1>StoreKeys</h1>
+ * A class that writes the public and private keys to the resources folder
+ * @author Michael Kyeyune
+ * @since 2016-01-04
+ */
+public class StoreKeys
+{
+    private static final String CLIENT_KEYS_FOLDER = "src/main/resources/keys/client/";
+    private static final String SERVER_KEYS_FOLDER = "src/main/resources/keys/server/";
+
+    public static void main(String[] args)
+    {
+        KeyPair clientKeyPair = RSA.generateKeyPair(2048);
+        KeyPair serverKeyPair = RSA.generateKeyPair(2048);
+
+        //save both client and server public keys in their key collections
+        STORE.savePublicKeyToFile(CLIENT_KEYS_FOLDER + "client-public.store", clientKeyPair.getPublic());
+        STORE.savePublicKeyToFile(SERVER_KEYS_FOLDER + "client-public.store", clientKeyPair.getPublic());
+
+        STORE.savePublicKeyToFile(SERVER_KEYS_FOLDER + "server-public.store", serverKeyPair.getPublic());
+        STORE.savePublicKeyToFile(CLIENT_KEYS_FOLDER + "server-public.store", serverKeyPair.getPublic());
+
+        //save the private keys in only the expected key collections
+        STORE.savePrivateKeyToFile(CLIENT_KEYS_FOLDER + "client-private.store", clientKeyPair.getPrivate());
+        STORE.savePrivateKeyToFile(SERVER_KEYS_FOLDER + "server-private.store", serverKeyPair.getPrivate());
+    }
+}
